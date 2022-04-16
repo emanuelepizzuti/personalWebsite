78a79f99adce506e320285e4ab4e0024deedbbc1
Emanuele Pizzuti <emanuele.pizzuti96@gmail.com>
2022-04-16 11:26:07 +0200

    szb

--- website/script.js
+++ website/script.js
@@ -1,24 +1,37 @@
+var control = true;
+
 $(".switch").click(function() {
-  $("body, html, .switch, #bottomText").toggleClass("body-colormode");
-  $("header, .text, .text2, .linkUni, .contacts, .switch, #bottomText").toggleClass("text-colormode");
+      $("body, html").toggleClass("body-colormode");
 
-  if ($(".switch").hasClass("body-colormode")) {
-    $(".switch").html("<-- back to easy mode");
-    $(".text2").removeClass("noDisplay");
-    $(".text").addClass("noDisplay");
-    $(".contacts").addClass("noDisplay");
+      if (control) {
+        $(".switch").html("<-- back to easy mode");
+        $(".text2").removeClass("noDisplay");
+        $(".text").addClass("noDisplay");
+        $(".contacts").addClass("noDisplay");
 
-    $("body").gradientify({
-      gradients: [
-        { start: [237,85,101], stop: [218,68,83] },
-        { start: [70,207,176], stop: [52,188,157] },
-        { start: [52,152,219], stop: [41,128,185] }
-      ]
-    });
-  } else {
-    $(".switch").html("click here to switch to pro mode -->");
-    $(".text").removeClass("noDisplay");
-    $(".text2").addClass("noDisplay");
-    $(".contacts").removeClass("noDisplay");
-  }
-});
+        control = false;
+
+        // $("body, html").gradientify({
+        //   gradients: [{
+        //       start: [237, 85, 101],
+        //       stop: [218, 68, 83]
+        //     },
+        //     {
+        //       start: [70, 207, 176],
+        //       stop: [52, 188, 157]
+        //     },
+        //     {
+        //       start: [52, 152, 219],
+        //       stop: [41, 128, 185]
+        //     }
+        //   ]
+        // });
+      } else if (control == false){
+        $(".switch").html("click here to switch to pro mode -->");
+        $(".text").removeClass("noDisplay");
+        $(".text2").addClass("noDisplay");
+        $(".contacts").removeClass("noDisplay");
+        control = true;
+}
+
+      });
