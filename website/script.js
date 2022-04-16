45ac785851f4dc8b8c481504de1dd5e4c1a6cac5
Emanuele Pizzuti <emanuele.pizzuti96@gmail.com>
2022-04-16 18:31:04 +0200

    gfgnfbg bgb

--- website/script.js
+++ website/script.js
@@ -1,37 +1,60 @@
 var control = true;
 
 $(".switch").click(function() {
-      $("body, html").toggleClass("body-colormode");
+  $("body, html").toggleClass("body-colormode");
 
-      if (control) {
-        $(".switch").html("<-- back to easy mode");
-        $(".text2").removeClass("noDisplay");
-        $(".text").addClass("noDisplay");
-        $(".contacts").addClass("noDisplay");
+  if (control) {
+    $(".switch").html("<-- back to easy mode");
+    $(".text2").removeClass("noDisplay");
+    $(".text").addClass("noDisplay");
 
-        control = false;
 
-        // $("body, html").gradientify({
-        //   gradients: [{
-        //       start: [237, 85, 101],
-        //       stop: [218, 68, 83]
-        //     },
-        //     {
-        //       start: [70, 207, 176],
-        //       stop: [52, 188, 157]
-        //     },
-        //     {
-        //       start: [52, 152, 219],
-        //       stop: [41, 128, 185]
-        //     }
-        //   ]
-        // });
-      } else if (control == false){
-        $(".switch").html("click here to switch to pro mode -->");
-        $(".text").removeClass("noDisplay");
-        $(".text2").addClass("noDisplay");
-        $(".contacts").removeClass("noDisplay");
-        control = true;
-}
 
-      });
+    var timer = clearInterval(timer);
+
+    setInterval(changeImg, 1000);
+
+    function changeImg() {
+      // TO DO
+      // $(".spiderman").toggleClass("spidermanImg");
+      // $(".spiderman").html("🕸 🕸 🕸 🕸");
+      var x = document.getElementById("spiderman");
+      if (x.innerHTML === "spiderman") {
+        x.innerHTML = "🕷 🕷 🕷 🕷";
+      } else {
+        x.innerHTML = "spiderman";
+      }
+
+      var x = document.getElementById("pizza");
+      if (x.innerHTML === "pizza") {
+        x.innerHTML = "🍕 🍕";
+      } else {
+        x.innerHTML = "pizza";
+      }
+    }
+
+
+    control = false;
+  } else if (control == false) {
+
+    clearInterval(timer);
+    $(".switch").html("click here to switch to pro mode -->");
+    $(".text").removeClass("noDisplay");
+    $(".text2").addClass("noDisplay");
+    $(".contacts").removeClass("noDisplay");
+    control = true;
+
+  }
+});
+// You do this by setting up a boolean check flag like
+// var isRecursiveOn = true;  // by default it will be true
+// var interval;
+    // button.onclick = function () {
+       // if  (isRecursionOn) {  // check the flag
+            // isRecursiveOn = false;
+            // interval = setInterval(slideShow, 2000); // assigned to a variable
+       // } else {
+           // clearInterval(interval);
+           // isRecursiveOn = true; //reset it
+       // }
+    // }
