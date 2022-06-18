76584b0f07b0832dc78fe2012009ba523a6f9c93
Emanuele Pizzuti <emanuele.pizzuti96@gmail.com>
2022-06-18 12:54:48 +0200

    BIG CHANGES

--- website/script.js
+++ website/script.js
@@ -1,50 +1,62 @@
-var control = true;
-
 $(document).ready(function() {
 
-  function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
+  var control = true;
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
+
+  $(".switch-button").click(function() {
+    $("body, html").toggleClass("body-colormode");
+    if (control) {
+      $(".switch-button").addClass("switch-button-white");
+      $(".nav-menu").addClass("body-colormode");
+      $(".text2").removeClass("noDisplay");
+      $(".text").addClass("noDisplay");
+      control = false;
+    } else if (control == false) {
+      $(".switch-button").removeClass("switch-button-white");
+      $(".nav-menu").removeClass("body-colormode");
+      $(".text").removeClass("noDisplay");
+      $(".text2").addClass("noDisplay");
+      $(".contacts").removeClass("noDisplay");
+      control = true;
+    }
+  });
+
+  // QUI IL TESTO CHE CAMBIA CON LE COSE CHE CI PIACCIONO
+  setInterval(changeText, 4000);
+  setInterval(changeText2, 5000);
+
+  function changeText2() {
+    var arr = ["super hero is spiderman", "tv show is the office", "anime is Attack on Titan"];
+    var x = document.getElementById("spiderman");
+    if (x.innerHTML === "super hero is spiderman") {
+      x.innerHTML = arr[1];
+    } else if (x.innerHTML === "tv show is the office") {
+      x.innerHTML = arr[2];
+    } else if (x.innerHTML === "anime is Attack on Titan") {
+      x.innerHTML = arr[0];
     }
   }
 
-  $(".text").lettering('words');
-  setInterval(changeImg, 2000);
-
-  function changeImg() {
-    var x = document.getElementById("spiderman");
-    if (x.innerHTML === "spiderman") {
-      x.innerHTML = "🕷 🙋‍♂️ 🕷 ‍🙋‍♂️";
-    } else {
-      x.innerHTML = "spiderman";
-    }
-
+  function changeText() {
+    var arr2 = ["pizza", "coffee", "LRDL"];
     var x = document.getElementById("pizza");
     if (x.innerHTML === "pizza") {
-      x.innerHTML = "🍕 🍕";
-    } else {
-      x.innerHTML = "pizza";
+      x.innerHTML = arr2[1];
+    } else if (x.innerHTML === "coffee") {
+      x.innerHTML = arr2[2];
+    } else if (x.innerHTML === "LRDL") {
+      x.innerHTML = arr2[0];
     }
-  }
-});
-
-$(".switch-button").click(function() {
-  $("body, html").toggleClass("body-colormode");
-
-  if (control) {
-    $(".switch-button").addClass("switch-button-white");
-    $(".text2").removeClass("noDisplay");
-    $(".text").addClass("noDisplay");
-
-    control = false;
-  } else if (control == false) {
-    $(".switch-button").removeClass("switch-button-white");
-    $(".text").removeClass("noDisplay");
-    $(".text2").addClass("noDisplay");
-    $(".contacts").removeClass("noDisplay");
-    control = true;
   }
 });
